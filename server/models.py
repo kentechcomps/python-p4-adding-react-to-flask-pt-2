e4372c0366e3c3af818891bf50cb3d9dcdba3753
user <mutukukennedy34@gmail.com>
2023-09-28 12:06:55 +0300

    flaskreact

--- server/models.py
+++ server/models.py
@@ -1,7 +1,14 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy import MetaData
 
-db = SQLAlchemy()
+
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+
+db = SQLAlchemy(metadata=metadata)
+
 
 class Movie(db.Model, SerializerMixin):
     __tablename__ = 'movies'
